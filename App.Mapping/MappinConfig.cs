71e47056d08eff39b1cbc7acc89da263f8a832f0
Mardc89 <mardc0602@gmail.com>
2025-10-26 14:08:17 -0500

    Agregando MappingTests

---
+++ App.Mapping/MappinConfig.cs
@@ -0,0 +1,25 @@
+﻿using App.DTO;
+using App.Entities.POCO;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Mapping
+{
+
+
+    public static class MappingConfig
+    {
+        public static void RegisterMapping(TypeAdapterConfig config)
+        {
+            #region Person
+            config.NewConfig<Person, PersonDTO>().TwoWays();
+            #endregion
+
+        }
+    }
+
+}
